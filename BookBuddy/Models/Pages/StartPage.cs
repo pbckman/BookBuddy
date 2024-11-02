62920d9599b28f6dad252eddb4f88367af285dbd
Alina Garifjanova <alina.garifjanova@yh.nackademin.se>
2024-11-02 14:17:54 +0100

    Created styles for sign in, sign up adn authlayout

--- BookBuddy/Models/Pages/StartPage.cs
+++ BookBuddy/Models/Pages/StartPage.cs
@@ -11,6 +11,7 @@ namespace BookBuddy.Models.Pages
     [
         typeof(ErrorPage),
         typeof(XmlSitemap),
+        typeof(CookieInfoPage),
     ])]
 
     public class StartPage : SitePageData
