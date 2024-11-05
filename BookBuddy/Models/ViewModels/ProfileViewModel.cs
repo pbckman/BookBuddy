f8d51ba26513a8a5c6e9e5e7baa49171091df96d
pbckman <143405511+pbckman@users.noreply.github.com>
2024-11-05 18:47:24 +0100

    Meta description added to auth, account and profile views. Styling added to profile views

--- BookBuddy/Models/ViewModels/ProfileViewModel.cs
+++ BookBuddy/Models/ViewModels/ProfileViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace BookBuddy.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookBuddy.Models.ViewModels
 {
     public class ProfileViewModel
     {
+        [Required(ErrorMessage = "You must enter a name")]
+        [MinLength(2, ErrorMessage = "A valid name is required")]
+        [Display(Name = "Name", Prompt = "Enter your name")]
         public string? FirstName { get; set; } = string.Empty;
         public string? LastName { get; set; } = string.Empty;
     }
--- BookBuddy/Models/ViewModels/UpdateUserViewModel.cs
+++ BookBuddy/Models/ViewModels/UpdateUserViewModel.cs
@@ -6,15 +6,18 @@ namespace BookBuddy.Models.ViewModels
     {
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Current password", Prompt = "Enter your current password")]
         public string? CurrentPassword { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "New password", Prompt = "Enter your new password")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
 
         public string? NewPassword { get; set; }
 
         [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password", Prompt = "Enter your new password again")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string? ConfirmNewPassword { get; set; }
 
