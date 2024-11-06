19486200a921545a776b5cc141b329f48aa40bb3
KristofferStrom <70570557+KristofferStrom@users.noreply.github.com>
2024-11-06 15:29:01 +0100

    quiz in progress

---
+++ BookBuddy/Controllers/QuizPageController.cs
@@ -0,0 +1,19 @@
+﻿using BookBuddy.Models.Pages;
+using BookBuddy.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookBuddy.Controllers
+{
+    public class QuizPageController : PageControllerBase<QuizPage>
+    {
+ 
+        public IActionResult Index(QuizPage currentPage)
+        {
+            var model = new QuizPageViewModel(currentPage, null);
+
+        
+
+            return View(model);
+        }
+    }
+}
---
+++ BookBuddy/Models/ViewModels/QuizPageViewModel.cs
@@ -0,0 +1,8 @@
+﻿using BookBuddy.Models.Pages;
+
+namespace BookBuddy.Models.ViewModels
+{
+    public class QuizPageViewModel(QuizPage currentPage, SiteSettingsPage siteSettingsPage) : PageViewModel<QuizPage>(currentPage, siteSettingsPage)
+    {
+    }
+}
