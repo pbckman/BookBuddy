624fc5b19f027da2153ac7d8a79f828a057b1a65
pbckman <143405511+pbckman@users.noreply.github.com>
2024-11-22 08:39:59 +0100

    Post merge fix

--- BookBuddy/Models/Pages/StartPage.cs
+++ BookBuddy/Models/Pages/StartPage.cs
@@ -7,11 +7,16 @@ namespace BookBuddy.Models.Pages
     GUID = "F7A0B723-F6D5-498D-AAAB-69B7C69D19A2",
     GroupName = GroupNames.Specialized
     )]
-    [AvailableContentTypes(Availability.Specific , Include =
+    [AvailableContentTypes(Availability.Specific, Include =
     [
         typeof(ErrorPage),
         typeof(XmlSitemap),
-        typeof(BooksPage)
+        typeof(BooksPage),
+        typeof(AvailableBooksPage),
+        typeof(ExamplePage),
+        typeof(CookieInfoPage),
+        typeof(SiteSettingsPage)
+
     ])]
 
     public class StartPage : SitePageData
